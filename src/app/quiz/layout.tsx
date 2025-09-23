25fe91d1e345d23f120bf475ec976b960466a00c
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-23 15:47:37 +0000

    diminua mais

--- src/app/quiz/layout.tsx
+++ src/app/quiz/layout.tsx
@@ -6,7 +6,7 @@ export default function QuizPagesLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex w-full flex-col items-center justify-center bg-background p-4 sm:p-8">
+    <main className="flex w-full flex-col items-center justify-center bg-background px-4 sm:px-8">
       <div className="w-full max-w-4xl">{children}</div>
     </main>
   );
